ff8447325a61d0ec1edf4ab00150af84580a6086
shimulsust999 <shimulsust999@gmail.com>
2023-03-31 06:20:22 +0600

    setting up routing

--- pages/component/Post.tsx
+++ pages/component/Post.tsx
@@ -1,31 +1,34 @@
+import Link from "next/link";
 import React, { Fragment } from "react";
 
 const Post = () => {
   return (
     <Fragment>
-      <div className="lws-card">
-        <a href="post.html">
-          <img src="/git.webp" className="lws-card-image" alt="" />
-        </a>
-        <div className="p-4">
-          <div className="lws-card-header">
-            <p className="lws-publishedDate">2023-05-01</p>
-            <p className="lws-likeCount">
-              <i className="fa-regular fa-thumbs-up"></i>100
-            </p>
-          </div>
-          <a href="post.html" className="lws-postTitle">
-            {" "}
-            Top Github Alternatives{" "}
+      <Link href="/component/PostDetails">
+        <div className="lws-card">
+          <a href="post.html">
+            <img src="/git.webp" className="lws-card-image" alt="" />
           </a>
-          <div className="lws-tags">
-            <span>#python,</span> <span>#tech,</span> <span>#git</span>
-          </div>
-          <div className="flex gap-2 mt-4">
-            <span className="lws-badge"> Saved </span>
+          <div className="p-4">
+            <div className="lws-card-header">
+              <p className="lws-publishedDate">2023-05-01</p>
+              <p className="lws-likeCount">
+                <i className="fa-regular fa-thumbs-up"></i>100
+              </p>
+            </div>
+            <a href="post.html" className="lws-postTitle">
+              {" "}
+              Top Github Alternatives{" "}
+            </a>
+            <div className="lws-tags">
+              <span>#python,</span> <span>#tech,</span> <span>#git</span>
+            </div>
+            <div className="flex gap-2 mt-4">
+              <span className="lws-badge"> Saved </span>
+            </div>
           </div>
         </div>
-      </div>
+      </Link>
     </Fragment>
   );
 };
--- pages/component/PostDetails.tsx
+++ pages/component/PostDetails.tsx
@@ -1,19 +1,23 @@
 import React, { Fragment } from "react";
 import Description from "./Description";
 import RelatedPost from "./RelatedPost";
+import Link from "next/link";
 
 const PostDetails = () => {
   return (
     <Fragment>
-      <div className="container mt-8">
-        <a
-          href="index.html"
-          className="inline-block text-gray-600 home-btn"
-          id="lws-goHome"
-        >
-          <i className="mr-2 fa-solid fa-house"></i>Go Home
-        </a>
-      </div>
+      <Link href="/">
+        <div className="container mt-8">
+          <a
+            href="index.html"
+            className="inline-block text-gray-600 home-btn"
+            id="lws-goHome"
+          >
+            <i className="mr-2 fa-solid fa-house"></i>Go Home
+          </a>
+        </div>
+      </Link>
+
       <section className="post-page-container">
         <Description />
         <RelatedPost />
